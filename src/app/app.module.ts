62ddbc1dcf0815eb85b468db88c6e3ff1f7cecc2
Wesley43S <wesleysimoes273s@gmail.com>
2024-11-17 07:56:41 -0300

    06/11 inicio 17/11 F5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagInicialComponent } from './componentes/pag-inicial/pag-inicial.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AgendarServicosComponent } from './componentes/agendar-servicos/agendar-servicos.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagInicialComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    LoginComponent,
+    AgendarServicosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
